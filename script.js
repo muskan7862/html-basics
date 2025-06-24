b2f7f985142eede861ac9e573edbc6be1954152e
muskan7862 <muskanfathima0918@gmail.com>
2025-06-24 12:52:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// syntax in javascript
+//Let, Const, Var are used to declare variables in javascript
+let a=10;
+//let is block scoped, cannot be redeclared in the same scope
+
+const b=20;
+//const is block scoped and cannot be reassigned
+
+var c=30;
+// var is funtion scoped, can be redeclared in the same scope
+console.log(a,b,c)
+// console.log will output the values of a,b, and c
+// Output: 10 20 30
+
