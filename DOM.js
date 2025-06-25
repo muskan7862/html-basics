6b858397e64ea16e0d83960cac4e1dc8250ae583
muskan7862 <muskanfathima0918@gmail.com>
2025-06-25 15:14:37 +0530

    Create DOM.js

---
+++ DOM.js
@@ -0,0 +1,12 @@
+//DOM in JavaScript
+document.body.style.backgroundColor = "aqua";
+document.title = "DOM in JavaScript";
+document.getElementById("header").innerHTML = "Hello World!";
+document.getElementById("paragraph").style.color = "red";
+document.getElementById("paragraph").style.fontSize = "30px";
+document.getElementsByClassName("ds")[1].style.color = "red";
+document.getElementsByClassName("ds")[2].style.color = "brown";
+document.getElementsByClassName("ds")[3].style.color = "orange";
+document.getElementsByClassName("ds")[1].style.fontSize = "20PX";
+document.getElementsByClassName("ds")[2].style.fontSize = "20PX";
+document.getElementsByClassName("ds")[3].style.fontSize= "20PX";
