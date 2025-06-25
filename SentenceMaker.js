df3a3ff7ffd718b07b347495881c9f84237a892a
muskan7862 <muskanfathima0918@gmail.com>
2025-06-25 11:40:35 +0530

    Create SentenceMaker.js

---
+++ SentenceMaker.js
@@ -0,0 +1,19 @@
+let adjective = "fuzzy";
+let noun = "dragon";
+let verb = "angry";
+let place = "mountain";
+let adjective2 = "sparkly";
+let noun2 = "marshmallows";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "slimy";
+noun = "goblin";
+verb = "scared";
+place = "swamp";
+adjective2 = "greasy";
+noun2 = "cupcakes";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
